b6db3e4da43d64bc2ac0cea48766041d83f3935c
Erik Zapata <zapatabrandon@gmail.com>
2018-12-02 12:00:43 -0500

    Add scene and Unit Pieces place holders

---
+++ Assets/src/Pieces/Unit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using King.Map;
+
+namespace King.Pieces
+{
+    public class Unit
+    {
+        public int Team { get; set; }
+        public int MaxMove { get; set; }
+        public byte MoveType { get; set; }
+
+        public Unit()
+        {
+            Team = 0;
+            MaxMove = 4;
+            MoveType = TileValues.WALK;
+        }
+    }
+}
