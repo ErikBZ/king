2bd8839a113ad7a0fcc944b62b75c4587b9da67d
Erik Zapata <zapatabrandon@gmail.com>
2018-12-15 22:01:22 -0500

    Create simple UI Manager for viewing tile info

---
+++ Assets/UI/Scripts/DetailUIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Tilemaps;
+using King.Pieces;
+using King.Map;
+
+public class DetailUIManager : MonoBehaviour {
+
+    public Text Info;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+    public void UpdateDetailePanel(MapTile tile, TileState state)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.Append(string.Format("{0}\n", tile.TileType.ToString()));
+        sb.Append(string.Format("Defense: {0}\n", tile.BaseDefBoost));
+        sb.Append(string.Format("Avoid: {0}\n", tile.BaseAvoidBoost));
+        sb.Append(string.Format("Additional Cost: {0}\n", tile.AdditionalMoveCost));
+
+        Info.text = sb.ToString();
+    }
+}
