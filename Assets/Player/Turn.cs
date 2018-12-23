6738a92afeee9d5b2a05e8fe06fe642296fea950
Erik Zapata <zapatabrandon@gmail.com>
2018-12-23 11:30:32 -0500

    Create Turn script to test different approaches

--- Assets/Player/Turn.cs
+++ Assets/Player/Turn.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.EventSystems;
 using King.Map;
 
-public class Turn : MonoBehaviour {
+public class Turn : MonoBehaviour
+{
 
     // Put UI manager here
 
@@ -19,17 +21,17 @@ public class Turn : MonoBehaviour {
     }
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        PlayerTurnIdle();
-	}
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //        ShowLandInfoOnHover();
+    }
 
     // when nothing is select this is what the player is allowed to do
-    private void PlayerTurnIdle()
+    private void ShowLandInfoOnHover()
     {
         var point = new Vector3Int(0, 0, 0);
 
@@ -45,4 +47,35 @@ public class Turn : MonoBehaviour {
             // clear UI Manager
         }
     }
+
+    public void OnMouseDown()
+    {
+        Debug.Log("Hello world");
+    }
+
+    // This won't work since it can't receive the event
+    //public void OnPointerDown(PointerEventData eventData)
+    //{
+    //    Debug.Log("Event handler works");
+    //    // IDK what the middle button might do
+    //    // this pushes events on the stack
+    //    if (eventData.button == PointerEventData.InputButton.Left)
+    //    {
+    //        Debug.Log("Trace");
+    //        var point = new Vector3Int(0, 0, 0);
+    //        if(TileManager.TryGetTilePoint(eventData.pressPosition, out point))
+    //        {
+    //            UIManager.UpdateDetailePanel(TileManager.tilemap.GetTile<MapTile>(point), TileManager.GetTileState(point));
+    //        }
+    //        else
+    //        {
+
+    //        }
+    //    }
+    //    // High level this pops the stack
+    //    else if (eventData.button == PointerEventData.InputButton.Right)
+    //    {
+
+    //    }
+    //}
 }
