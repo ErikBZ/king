26aeba4a1b9bb6f17bf8c2c9879ea54b87680081
Erik Zapata <zapatabrandon@gmail.com>
2018-12-26 14:52:28 -0500

    Creates Unit Reference Object

---
+++ Assets/src/Pieces/UnitReference.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace King.Pieces
+{
+
+    [CreateAssetMenu(fileName = "UnitReference", menuName = "king/UnitReference", order = 0)]
+    public class UnitReference : ScriptableObject
+    {
+        public Unit Unit;
+    }
+}
