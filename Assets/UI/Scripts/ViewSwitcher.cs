8319443d0e1b7e2d1d8b8b8c830c61aed01e2a37
Erik Zapata <zapatabrandon@gmail.com>
2018-12-27 20:59:05 -0500

    Creates prefab for start menu, and ViewSwitcher script

---
+++ Assets/UI/Scripts/ViewSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewSwitcher : MonoBehaviour
+{
+    public Toggle MenuToggleObject;
+    public Toggle MapToggleObject;
+
+
+    void Start()
+    {
+        this.GetComponentInChildren<Text>().text = MenuToggleObject.isOn ? "Menu" : "Map";
+    }
+
+    public void ToggleButton()
+    {
+        MenuToggleObject.isOn = !MenuToggleObject.isOn;
+        MapToggleObject.isOn = !MapToggleObject.isOn;
+
+        this.GetComponentInChildren<Text>().text = MenuToggleObject.isOn ? "Menu" : "Map";
+    }
+
+}
