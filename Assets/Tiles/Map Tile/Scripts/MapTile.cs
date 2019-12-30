8e4a918a95d748cf0c6a07caa580ec15cfddca95
Erik Zapata <zapatabrandon@gmail.com>
2019-12-30 15:52:15 -0500

    Fix map tile assets

--- Assets/Tiles/Map Tile/Scripts/MapTile.cs
+++ Assets/Tiles/Map Tile/Scripts/MapTile.cs
@@ -20,9 +20,18 @@ namespace UnityEngine.Tilemaps
         public int BaseDefBoost;
         public int BaseAvoidBoost;
         public byte MoveConstraints;
-        // All moves cost 1
-        public byte AdditionalMoveCost;
-        public TileUnityEvent tileEvent;
+        // All moves cost 1 by default
+        public byte AdditionalMoveCost = 1;
+        public TileUnityEvent TileEvent;
 
+        public MapTile()
+        {
+            // Might not need this if i'm using the MoveConstraints
+            TileType = TileType.Normal;
+            BaseDefBoost = 0;
+            BaseAvoidBoost = 0;
+            MoveConstraints = TileValues.STANDARD;
+            TileEvent = null;
+        }
     }
 }
