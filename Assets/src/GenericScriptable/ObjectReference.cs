660a144ae97e480128398dc23bc092582407ce8b
Erik Zapata <zapatabrandon@gmail.com>
2019-12-30 20:54:00 -0500

    Add static methods for checking if an ObjectReference and it's value is non-null

--- Assets/src/GenericScriptable/ObjectReference.cs
+++ Assets/src/GenericScriptable/ObjectReference.cs
@@ -4,4 +4,21 @@
 public class ObjectReference : ScriptableObject
 {
     public object Reference;
+
+    public static bool NotEmpty(ObjectReference objRef)
+    {
+        return objRef != null && objRef.Reference != null;
+    }
+    public static bool NotEmpty<T>(ObjectReference objRef, out T obj)
+    {
+        if (objRef != null && objRef.Reference != null && objRef.Reference is T)
+        {
+            obj = (T)objRef.Reference;
+            return false;
+        } else
+        {
+            obj = default(T);
+            return true;
+        }
+    }
 }
