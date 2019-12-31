5b5eb3e693d940d64989346ab6cb08c93b4b25d4
Erik Zapata <zapatabrandon@gmail.com>
2019-12-30 20:59:39 -0500

    Initialize the chapter's map data by loading it from disk.

---
+++ Assets/Level/Chapter/InitTilemap.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using King.Level;
+using King.Map;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Newtonsoft.Json;
+using King.Utils;
+
+[RequireComponent(typeof(TileManager))]
+public class InitTilemap : MonoBehaviour
+{
+    public ObjectReference LevelDataReference;
+    Tilemap MainMap;
+    TileManager TileManager;
+
+    void Start()
+    {
+        TileManager = GetComponent<TileManager>();
+        TileManager.enabled = false;
+
+        if (!ObjectReference.NotEmpty(LevelDataReference, out LevelData levelData))
+        {
+            string path = levelData.MapDataJsonPath;
+            MapData MapData = LoadMapData(path);
+
+            // lol this is so unnecessary  
+            Tilemap tilemap = GetComponentsInChildren<TilemapRenderer>()
+                .Where(tr => tr.sortingLayerName == "Map")
+                .Select(tr => tr.GetComponent<Tilemap>())
+                .First();
+
+            tilemap.ClearAllTiles(); 
+
+            ImportMap(MapData, tilemap);
+        }
+
+        TileManager.enabled = true;
+    }
+
+    MapData LoadMapData(string path)
+    {
+        string fullPath = Path.Combine(Application.dataPath, LevelSelectHelper.MapDirectory, path);
+        if (File.Exists(fullPath))
+        {
+            string data = File.ReadAllText(fullPath);
+            return JsonConvert.DeserializeObject<MapData>(data, new DictVec3IntStringConverter());
+        }
+        return new MapData();
+    }
+    void ImportMap(MapData data, Tilemap tilemap)
+    {
+        Dictionary<string, MapTile> mapTiles = AssetFinder.GetMappedInstances<MapTile>();
+        foreach(KeyValuePair<Vector3Int, string> kvp in data.Tiles)
+        {
+            tilemap.SetTile(kvp.Key, mapTiles[kvp.Value]);
+        }
+    }
+}
--- Assets/UnityEditor/LevelSelectHelper.cs
+++ Assets/UnityEditor/LevelSelectHelper.cs
@@ -5,10 +5,11 @@ using System.Linq;
 
 public static class LevelSelectHelper
 {
+    public const string MapDirectory = "Level/Maps";
     // checks the "maps" sub directory for maps
     public static string[] GetAllMaps()
     {
-        DirectoryInfo dir = new DirectoryInfo(Path.Combine(Application.dataPath, "Level", "Maps"));
+        DirectoryInfo dir = new DirectoryInfo(Path.Combine(Application.dataPath, MapDirectory));
         FileInfo[] files = dir.GetFiles("*.json");
         return files.Select(f => f.Name).ToArray();
     }
