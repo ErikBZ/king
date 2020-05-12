62be64dabbae41ac27573709a82858e99deda826
Erik Zapata <zapatabrandon@gmail.com>
2020-05-11 21:27:40 -0400

    Remove duplicate code, and unused variables

--- Assets/Scenes/Chapter/Code/Player/MouseActions.cs
+++ Assets/Scenes/Chapter/Code/Player/MouseActions.cs
@@ -15,21 +15,8 @@ public class MouseActions : MonoBehaviour
     public ObjectReference tilesToHighlight;
     public CustomEvent cursorMovedEvent;
 
-    private TileHighlighter Highlighter;
     private Vector3Int oldCursorPos;
 
-    private void Awake()
-    {
-        Highlighter = new TileHighlighter(TileManager.tilemap);
-    }
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         var mousePos = Input.mousePosition;
@@ -45,53 +32,4 @@ public class MouseActions : MonoBehaviour
             oldCursorPos = currCursorPos;
         }
     }
-
-    // when nothing is select this is what the player is allowed to do
-    private void ShowLandInfoOnHover()
-    {
-        var point = new Vector3Int(0, 0, 0);
-
-        if (TileManager.TryGetTilePoint(Input.mousePosition, out point))
-        {
-            Highlighter.PointHighlight(point);
-
-            // ui manager show board point
-            UIManager.UpdateDetailePanel(TileManager.tilemap.GetTile<MapTile>(point), TileManager.GetTileState(point));
-        }
-        else
-        {
-            // clear UI Manager
-        }
-    }
-
-    public void OnMouseDown()
-    {
-        Debug.Log("Hello world");
-    }
-
-    // This won't work since it can't receive the event
-    //public void OnPointerDown(PointerEventData eventData)
-    //{
-    //    Debug.Log("Event handler works");
-    //    // IDK what the middle button might do
-    //    // this pushes events on the stack
-    //    if (eventData.button == PointerEventData.InputButton.Left)
-    //    {
-    //        Debug.Log("Trace");
-    //        var point = new Vector3Int(0, 0, 0);
-    //        if(TileManager.TryGetTilePoint(eventData.pressPosition, out point))
-    //        {
-    //            UIManager.UpdateDetailePanel(TileManager.tilemap.GetTile<MapTile>(point), TileManager.GetTileState(point));
-    //        }
-    //        else
-    //        {
-
-    //        }
-    //    }
-    //    // High level this pops the stack
-    //    else if (eventData.button == PointerEventData.InputButton.Right)
-    //    {
-
-    //    }
-    //}
 }
--- Assets/Scenes/Chapter/Code/Turn.cs
+++ Assets/Scenes/Chapter/Code/Turn.cs
@@ -5,16 +5,10 @@ using King.MapSystem.Tiles;
 public class Turn : MonoBehaviour
 {
 
-    // Put UI manager here
-
-    public TileManager TileManager;
-    public DetailUIManager UIManager;
-
-    private TileHighlighter Highlighter;
-
+    // TODO add state mangement for a players turn
     private void Awake()
     {
-        Highlighter = new TileHighlighter(TileManager.tilemap);
+
     }
 
     // Use this for initialization
@@ -24,55 +18,11 @@ public class Turn : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        //        ShowLandInfoOnHover();
-    }
 
-    // when nothing is select this is what the player is allowed to do
-    private void ShowLandInfoOnHover()
-    {
-        var point = new Vector3Int(0, 0, 0);
-
-        if (TileManager.TryGetTilePoint(Input.mousePosition, out point))
-        {
-            Highlighter.PointHighlight(point);
-
-            // ui manager show board point
-            UIManager.UpdateDetailePanel(TileManager.tilemap.GetTile<MapTile>(point), TileManager.GetTileState(point));
-        }
-        else
-        {
-            // clear UI Manager
-        }
     }
 
     public void OnMouseDown()
     {
         Debug.Log("Hello world");
     }
-
-    // This won't work since it can't receive the event
-    //public void OnPointerDown(PointerEventData eventData)
-    //{
-    //    Debug.Log("Event handler works");
-    //    // IDK what the middle button might do
-    //    // this pushes events on the stack
-    //    if (eventData.button == PointerEventData.InputButton.Left)
-    //    {
-    //        Debug.Log("Trace");
-    //        var point = new Vector3Int(0, 0, 0);
-    //        if(TileManager.TryGetTilePoint(eventData.pressPosition, out point))
-    //        {
-    //            UIManager.UpdateDetailePanel(TileManager.tilemap.GetTile<MapTile>(point), TileManager.GetTileState(point));
-    //        }
-    //        else
-    //        {
-
-    //        }
-    //    }
-    //    // High level this pops the stack
-    //    else if (eventData.button == PointerEventData.InputButton.Right)
-    //    {
-
-    //    }
-    //}
 }
